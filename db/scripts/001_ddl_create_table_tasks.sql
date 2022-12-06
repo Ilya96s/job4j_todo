887d7a394b5fd2a71aaf2495f52ed9ab815c00db
Ilya <i.kaltygin@icloud.com>
2022-12-06 15:05:51 +0700

    Реализовал скрипт для создания таблицЫ tasks

---
+++ db/scripts/001_ddl_create_table_tasks.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS tasks (
+    id SERILA PRIMARY KEY,
+    description text,
+    created TIMESTAMP,
+    done BOOLEAN
+);
+
+comment on table tasks is 'Таблица с заданиями';
+comment on column tasks.id is 'Идентификатор задания';
+comment on column tasks.description is 'Описание задания';
+comment on column tasks.created is 'Дата создания задания';
+comment on column tasks.done is 'Статус задания';
