4b35f27ac805fb0a4bf611cbd29b160fac8e3409
Ilya <i.kaltygin@icloud.com>
2022-12-08 22:29:06 +0700

    Реализовал слой сервисов

---
+++ src/main/java/ru/job4j/todo/service/SimpleTaskService.java
@@ -0,0 +1,88 @@
+package ru.job4j.todo.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.job4j.todo.model.Task;
+import ru.job4j.todo.repository.TaskRepository;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * SimpleTaskService - реализация сервиса по работе с задачами.
+ *
+ * @author Ilya Kaltygin
+ */
+
+@Service
+@AllArgsConstructor
+public class SimpleTaskService implements TaskService {
+
+    /**
+     * Хранилище задач
+     */
+    private final TaskRepository taskRepository;
+
+    /**
+     * Добавить задачу в базу данных.
+     * @param task задача.
+     * @return Optional.of(task) если добавили, иначе Optional.empty().
+     */
+    @Override
+    public Task add(Task task) {
+        return taskRepository.add(task);
+    }
+
+    /**
+     * Изменить задачу в базе данных.
+     * @param id id задача, которую нужно изменить.
+     * @param task задача.
+     */
+    @Override
+    public boolean replace(int id, Task task) {
+        return taskRepository.replace(id, task);
+    }
+
+    /**
+     * Удалить задачу из базы данных.
+     * @param id id задачи, которую нужно удалить.
+     */
+    @Override
+    public boolean delete(int id) {
+        return taskRepository.delete(id);
+    }
+
+    /**
+     * Найти все задачи в базе данных.
+     * @return список задач.
+     */
+    @Override
+    public List<Task> findAll() {
+        return taskRepository.findAll();
+    }
+
+    /**
+     * Найти задачу по id.
+     * @param id id.
+     * @return Optional.of() если задача с таким id найдено, иначе Optional.empty().
+     */
+    @Override
+    public Optional<Task> findById(int id) {
+        return taskRepository.findById(id);
+    }
+
+    /**
+     * Найти все задачи со статусом true.
+     * @return список задач.
+     */
+    public List<Task> findReadyTasks() {
+        return taskRepository.findReadyTasks();
+    }
+
+    /**
+     * Найти последние добавленные задачи.
+     * @return список задач.
+     */
+    public List<Task> findNotReadyTasks() {
+        return taskRepository.findNotReadyTasks();
+    }
+}
---
+++ src/main/java/ru/job4j/todo/service/TaskService.java
@@ -0,0 +1,58 @@
+package ru.job4j.todo.service;
+
+import ru.job4j.todo.model.Task;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * TaskService - интерфейс, описывающий бизнес логику по работе с задачами
+ *
+ * @author Ilya Kaltygin
+ */
+public interface TaskService {
+
+    /**
+     * Добавить задачу в базу данных.
+     * @param task задача.
+     * @return Optional.of(task) если добавили, иначе Optional.empty().
+     */
+    Task add(Task task);
+
+    /**
+     * Изменить задачу в базе данных.
+     * @param id id задача, которую нужно изменить.
+     * @param task задача.
+     */
+    boolean replace(int id, Task task);
+
+    /**
+     * Удалить задачу из базы данных.
+     * @param id id задачи, которую нужно удалить.
+     */
+    boolean delete(int id);
+
+    /**
+     * Найти все задачи в базе данных.
+     * @return список задач.
+     */
+    List<Task> findAll();
+
+    /**
+     * Найти задачу по id.
+     * @param id id.
+     * @return Optional.of() если задача с таким id найдено, иначе Optional.empty().
+     */
+    Optional<Task> findById(int id);
+
+    /**
+     * Найти все задачи со статусом true.
+     * @return список задач.
+     */
+    List<Task> findReadyTasks();
+
+    /**
+     * Найти последние добавленные задачи.
+     * @return список задач.
+     */
+    List<Task> findNotReadyTasks();
+}
