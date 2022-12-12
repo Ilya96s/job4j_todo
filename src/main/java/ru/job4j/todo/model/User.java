1b5e2adfe9a84f922b5b46111c53756867d9e793
Ilya <i.kaltygin@icloud.com>
2022-12-12 21:30:35 +0700

    Создал модель данных User

---
+++ src/main/java/ru/job4j/todo/model/User.java
@@ -0,0 +1,41 @@
+package ru.job4j.todo.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import javax.persistence.*;
+
+/**
+ * User - модель данных, описывающая пользователя
+ *
+ * @author Ilya Kaltygin
+ */
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "todo_user")
+public class User {
+
+    /**
+     * id пользователя.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private int id;
+
+    /**
+     * имя пользователя.
+     */
+    private String name;
+
+    /**
+     * логин пользователя
+     */
+    private String login;
+
+    /**
+     * пароль пользователя
+     */
+    private String password;
+}
