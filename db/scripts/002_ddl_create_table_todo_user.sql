819f58564f2441bdaea6686de7594f9f6ec70b29
Ilya <i.kaltygin@icloud.com>
2022-12-12 21:29:47 +0700

    Реализовал скрипт для создания таблицы todo_user

---
+++ db/scripts/002_ddl_create_table_todo_user.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS todo_user(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    login TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
+);
+
+comment on table todo_user is 'Таблица с пользователями';
+comment on column todo_user.id is 'Имя пользователя';
+comment on column todo_user.login is 'Логин пользователя';
+comment on column todo_user.password is 'Пароль пользователя';
